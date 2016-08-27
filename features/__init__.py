bd1620778ccda073a011a12248a75c22e961b6c0
Mike Bannister <mike@Williams-Mac-mini.local>
2016-08-27 10:25:09 -0600

    setup.py seems to be working, other files need to be updated

--- features/__init__.py
+++
@@ -1,6 +0,0 @@
-from cross_section import CrossSection
-from river_reach import RiverReach
-from culvert import Culvert
-from bridge import Bridge
-from lateral_weir import LateralWeir
-from junction import Junction
