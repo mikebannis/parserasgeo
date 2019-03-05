7e0b33805cc96873ffb62a408a2431a5af0cf687
mikebannis <mikebannis@gmail.com>
2019-03-05 08:02:07 -0700

    upgraded version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='parserasgeo', version='0.12b', 
+setup(name='parserasgeo', version='0.12c', 
       description='Read and write HEC-RAS geometry files', 
       author='Mike Bannister', author_email='mikebannis@gmail.com', 
       packages=['parserasgeo', 'parserasgeo.features'])
