46d69526ee260a0207fa5b766f86d02040bcd36c
Jorn Baayen <jorn.baayen@kisters-bv.nl>
2019-08-20 11:14:43 +0200

    Parse geometry and unsteady files from project file

--- parserasgeo/prprj.py
+++ parserasgeo/prprj.py
@@ -11,10 +11,11 @@ class ParseRASProject(object):
     def __init__(self, project_filename):
         self.project_title = None   # Full project name
         self.plan_files = []    # list of plan file extension: p01, p02, ..
+        self.geom_files = []    # list of geom file extension: g01, g02, ..
+        self.unsteady_files = []     # list of unsteady file extension: u01, u02, ...
 
         with open(project_filename, 'rt') as project_file:
             for line in project_file:
-                print(line)
                 fields = line[:-1].split('=')# Strip the newline
 
                 # lookout for lines missing =
@@ -27,11 +28,19 @@ class ParseRASProject(object):
                     self.project_title = value
                 elif var == 'Plan File':
                     self.plan_files.append(value)
+                elif var == 'Geom File':
+                    self.geom_files.append(value)
+                elif var == 'Unsteady File':
+                    self.unsteady_files.append(value)
 
     def __str__(self):
         s = 'Proj Title='+self.project_title+'\n'
         for plan in self.plan_files:
             s += 'Plan File='+plan+'\n'
+        for geom in self.geom_files:
+            s += 'Geom file='+geom+'\n'
+        for unsteady in self.unsteady_files:
+            s += 'Unsteady File='+unsteady+'\n'
         return s
 
 def main():
