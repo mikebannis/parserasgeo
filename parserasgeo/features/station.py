4d7f6441b1a5a26b9a879c170af9ba804451549f
Jesse VanderWees <jesse.vanderwees@kisters-bv.nl>
2019-10-17 22:25:36 +0200

    reuse instance var

--- parserasgeo/features/station.py
+++ parserasgeo/features/station.py
@@ -2,8 +2,8 @@ class Station(object):
     def __init__(self, station):
         self._raw_station = station
         self._id = station.strip()
-        self._is_interpolated = station.endswith("*")
-        self._value = float(station[:-1]) if self._is_interpolated else float(station)
+        self._is_interpolated = self._id.endswith("*")
+        self._value = float(self._id[:-1]) if self._is_interpolated else float(self._id)
 
     def __str__(self):
         return self._raw_station
